867f7f84fb19531f8822385eafbf5ff0739a1d7b
brunomairey <bruno.mairey@yahoo.fr>
2020-06-18 13:26:50 +0200

    basic with formular

--- basic.ts
+++ basic.ts
@@ -1,3 +1,9 @@
+
+function result() {
+    console.log("Goran");
+
+
+
 let arrayperson:Array<any> = [];
 
 class Person { // ①
@@ -9,40 +15,65 @@ constructor(name, age, jobtitle) { // ③
        this.age = age;
        this.jobtitle = jobtitle;
        arrayperson.push(this);
-       console.table(this);
-   }
+         }
    hellothere() { // ④
-       return `Hello there, My name is ${this.name} and I am ${this.age} years old, and I am a ${this.jobtitle}<hr>`;
+       return `Hello there, My name is ${this.name} and I am ${this.age} years old, and I am a ${this.jobtitle}<br>`;
    }
 };
-var Goran = new Person ("Goran", 5, "developer");
-var Bruno = new Person ("Bru", 6, "developer");
+// var Goran = new Person ("Goran", 5, "developer");
+// var Bruno = new Person ("Bru", 6, "developer");
 
-let i=0
-
-for (i=0; i<arrayperson.length; i++) {
-document.getElementById('basic1').innerHTML += arrayperson[i].hellothere();
-
-}
 
 class Job extends Person { // ①
-   salary;
-   joblocation;
+   salary :number;
+   joblocation :string;
     // ②
 
    constructor(name, age, jobtitle, salary, joblocation) {
        super(name, age, jobtitle); // ③
         this.salary = salary;
         this.joblocation = joblocation;
+       
+    
    }
+
+
 
    hellothere() { // ④
-       return  `${super.hellothere()} and and I get ${this.salary} every month, and I work in ${this.joblocation}`; // ⑤
+       return  `${super.hellothere()} and and I get ${this.salary} every month, and I work in ${this.joblocation}<hr>`; // ⑤
    }
 };
+ 
 
-var Goran2 = new Job("Goran", 5, "developer", 100, "Vienna");
-document.getElementById('basic2').innerHTML += Goran2.hellothere();
+// var Goran = new Job("Gogi", 88, "backenddeveloper", 300, "Vienna");
+// var Bruno = new Job("Bruno", 7, "developerweb", 200, "Munich");
+// document.getElementById('basic2').innerHTML += Goran.hellothere();
+
+var name2 = (<HTMLInputElement>document.getElementById("name")).value;
+     var age2 = Number((<HTMLInputElement>document.getElementById("age")).value);
+     var jobtitle2 = (document.getElementById("jobtitle").value);
+     var salary2 = (document.getElementById("salary").value);
+     var joblocation2 = (document.getElementById("joblocation").value);
+
+var newPerson = new Job(name2, age2, jobtitle2, salary2, joblocation2)
+
+console.table(arrayperson);
+
+let i=0
+
+for (i=0; i<arrayperson.length; i++) {
+// document.getElementById('basic1').innerHTML += arrayperson[i].hellothere();
+document.getElementById('result').innerHTML += arrayperson[i].hellothere()
+}
+
+
+
+
+    
+    
+     
+ }
+
 //Create a class based on the Person class and add some other properties like salary, jobLocation and a function that will call the 
 //function that is inside the Person class and add “and I get (salary) every month, and I work in (jobLocation)”
 
